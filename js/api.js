d4f1d62a6137fb25e0e6b57a985c30e517a6371a
giridhar9 <giridhar717@gmail.com>
2015-11-10 11:07:24 -0800

    project6

---
+++ js/api.js
@@ -0,0 +1,16 @@
+(function ($) {
+	'use strict';
+
+   /**
+    * Ajax-based random post fetching.
+    */
+
+	// Code goes here...
+
+   /**
+    * Ajax-based front-end post submissions.
+    */
+
+	 // Code goes here...
+
+}(jQuery));
